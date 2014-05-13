d85eebb4578f3bafaecb9b3c9be7b9f63a871bf2
Valentin GIRAUD <giraud.valentin@gmail.com>
2014-05-14 01:26:11 +0200

    commentaire fucntion

---
+++ admin/controllers/commentaire/commentaire.php
@@ -0,0 +1,26 @@
+<?php
+
+// On inclus le modèle néccéssaire
+require_once MODELS.'M_Commentaire.php';
+
+class Commentaire
+{
+    // Function relative aux Commentaires
+
+    static function get_commentaire($idProjet)
+    {
+        // Si on a un ID
+        if(!empty($idProjet))
+            $get_commentaire = M_Commentaire::get_commentaire($idProjet);
+
+        return $get_commentaire;
+    }
+
+    static function set_commentaire($idProjet, $idUtilisateur, $commentaire){
+        if(!empty($idProjet) && count($commentaire) && !empty($idUtilisateur)){
+            $set_commentaire = M_Commentaire::set_commentaire($idProjet, $idUtilisateur, $commentaire);
+        }
+
+        return $set_commentaire;
+    }
+}
