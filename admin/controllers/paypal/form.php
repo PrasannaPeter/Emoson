2d13a55c8948f6ba176713d02defe50b6e36c914
Vik <vik93150@gmail.com>
2014-05-27 19:38:26 +0200

    Nouveaux parametre bouton paypal

--- admin/controllers/paypal/form.php
+++ admin/controllers/paypal/form.php
@@ -1,6 +1,6 @@
-﻿<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="DLSD68DZREXWG">
-<input type="image" src="https://www.sandbox.paypal.com/fr_FR/FR/i/btn/btn_paynow_SM.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
+<input type="hidden" name="hosted_button_id" value="WDJKV2LW62KAC">
+<input type="image" src="https://www.sandbox.paypal.com/fr_FR/FR/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal - la solution de paiement en ligne la plus simple et la plus sécurisée !">
 <img alt="" border="0" src="https://www.sandbox.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
 </form>
