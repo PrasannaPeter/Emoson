d579d6d8cd0cacbd58318d615e10b2421e4dc736
PETER <peter.prasana@gmail.com>
2014-06-16 11:12:44 +0200

    emoson sql soundcloud table

---
+++ admin/controllers/youtubeVideo/connexionSoudcloud.php
@@ -0,0 +1,8 @@
+<script src="http://connect.soundcloud.com/sdk.js"></script>
+<script>
+// initialize client with app credentials
+SC.initialize({
+  client_id: 'e81b2e10ea9abad8f48cbef75335b0ef',
+  redirect_uri: 'http://localhost/emoson/admin/controllers/soundcloud/soundcloud.php'
+});
+</script>
--- etc/emoson.sql
+++ etc/emoson.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: May 27, 2014 at 05:59 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
+-- Client :  127.0.0.1
+-- Généré le :  Lun 16 Juin 2014 à 11:10
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,55 +17,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `emoson`
+-- Base de données :  `emoson`
 --
-CREATE DATABASE IF NOT EXISTS `emoson` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `emoson`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `commentaires`
+-- Structure de la table `commentaires`
 --
 
 CREATE TABLE IF NOT EXISTS `commentaires` (
-  `idCommentaire` int(11) NOT NULL AUTO_INCREMENT,
   `idUtilisateur` int(11) NOT NULL,
   `idProjet` int(11) NOT NULL,
-  `dateCommentaire` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `texteCommentaire` longtext NOT NULL,
-  PRIMARY KEY (`idCommentaire`),
+  `dateCommentaire` varchar(45) NOT NULL,
+  `texteCommentaire` varchar(45) NOT NULL,
+  PRIMARY KEY (`idUtilisateur`,`idProjet`),
   KEY `fk_utilisateurs_has_projets_projets1_idx` (`idProjet`),
   KEY `fk_utilisateurs_has_projets_utilisateurs1_idx` (`idUtilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
-
---
--- Dumping data for table `commentaires`
---
-
-INSERT INTO `commentaires` (`idCommentaire`, `idUtilisateur`, `idProjet`, `dateCommentaire`, `texteCommentaire`) VALUES
-(1, 2, 1, '2014-05-04 17:36:20', 'test'),
-(3, 2, 1, '2014-05-04 21:24:01', 'azeazeaze'),
-(4, 2, 1, '2014-05-04 21:24:28', 'teazeazrfdfgdfg'),
-(5, 2, 1, '2014-05-04 21:24:52', 'teazeazrfdfgdfg'),
-(6, 2, 1, '2014-05-04 21:26:07', 'teazeazrfdfgdfg'),
-(7, 2, 1, '2014-05-04 21:26:18', 'teazeazrfdfgdfg'),
-(8, 2, 1, '2014-05-04 21:28:51', 'testazeazehh !!! !'''),
-(9, 2, 1, '2014-05-04 21:32:26', 'teazeazrfdfgdfgteazeazrfdfgdfgteazeazrfdfgdfg'),
-(10, 2, 1, '2014-05-04 21:32:52', 'teazeaz rfdfgdfgte             azeazrfdfgdfgt'),
-(11, 2, 1, '2014-05-04 21:33:16', 'eazeazrfdfgdf                                '),
-(12, 2, 1, '2014-05-04 21:33:31', 'eazeazrfdfgdf                                '),
-(13, 2, 1, '2014-05-04 21:33:37', '4848'),
-(14, 2, 1, '2014-05-04 21:34:37', 'eazeazrfdfgdf                                '),
-(15, 2, 1, '2014-05-04 21:36:06', '&lt;a href=&quot;#&quot; class=&quot;delete&q'),
-(16, 2, 1, '2014-05-04 21:38:25', '&lt;a href=&quot;#&quot; class=&quot;delete&q'),
-(17, 2, 1, '2014-05-04 21:39:48', '&lt;a href=&quot;#&quot; class=&quot;delete&quot; data-confirm=&quot;Are you sure to delete this item?&quot;&gt;Delete&lt;/a&gt;\r\n'),
-(18, 2, 1, '2014-05-05 20:37:35', 'rzerzer');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comptes_lies`
+-- Structure de la table `comptes_lies`
 --
 
 CREATE TABLE IF NOT EXISTS `comptes_lies` (
@@ -78,7 +52,48 @@ CREATE TABLE IF NOT EXISTS `comptes_lies` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `entreprises`
+-- Structure de la table `compte_soundcloud`
+--
+
+CREATE TABLE IF NOT EXISTS `compte_soundcloud` (
+  `compte_soundcloud_id` int(100) NOT NULL AUTO_INCREMENT,
+  `compte_soundcloud_designer_id` int(100) NOT NULL,
+  `compte_soundcloud_soundcloud_id` int(100) NOT NULL,
+  PRIMARY KEY (`compte_soundcloud_id`),
+  KEY `compte_soundcloud_designer_id` (`compte_soundcloud_designer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `compte_soundcloud`
+--
+
+INSERT INTO `compte_soundcloud` (`compte_soundcloud_id`, `compte_soundcloud_designer_id`, `compte_soundcloud_soundcloud_id`) VALUES
+(5, 5, 92388047);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `designer_img`
+--
+
+CREATE TABLE IF NOT EXISTS `designer_img` (
+  `designer_img_id` int(100) NOT NULL AUTO_INCREMENT,
+  `designer_img_designer_id` int(100) NOT NULL,
+  `designer_img_url` varchar(100) NOT NULL,
+  PRIMARY KEY (`designer_img_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `designer_img`
+--
+
+INSERT INTO `designer_img` (`designer_img_id`, `designer_img_designer_id`, `designer_img_url`) VALUES
+(3, 5, '947163_10200581901941340_1395013270_n.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entreprises`
 --
 
 CREATE TABLE IF NOT EXISTS `entreprises` (
@@ -94,19 +109,12 @@ CREATE TABLE IF NOT EXISTS `entreprises` (
   `numSiretEntreprise` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idEntreprise`),
   KEY `fk_entreprises_utilisateurs1_idx` (`idUtilisateur`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `entreprises`
---
-
-INSERT INTO `entreprises` (`idEntreprise`, `raisonSocialeEntreprise`, `secteurEntreprise`, `siteWebEntreprise`, `adresseEntreprise`, `villeEntreprise`, `CPEntreprise`, `typeEntreprise`, `idUtilisateur`, `numSiretEntreprise`) VALUES
-(1, 'Emoson', 'Informatique', 'www.emoson.fr', 'CitÃ© paradis ', 'Paris', '750000', 'SARL', 2, '1245121.240');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fichiers_lies`
+-- Structure de la table `fichiers_lies`
 --
 
 CREATE TABLE IF NOT EXISTS `fichiers_lies` (
@@ -123,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `fichiers_lies` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pack`
+-- Structure de la table `pack`
 --
 
 CREATE TABLE IF NOT EXISTS `pack` (
@@ -136,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `pack` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `pack`
+-- Contenu de la table `pack`
 --
 
 INSERT INTO `pack` (`idPack`, `titrePack`, `descPack`, `prixPack`, `positionPack`) VALUES
@@ -147,19 +155,13 @@ INSERT INTO `pack` (`idPack`, `titrePack`, `descPack`, `prixPack`, `positionPack
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projets`
+-- Structure de la table `projets`
 --
 
 CREATE TABLE IF NOT EXISTS `projets` (
   `idProjet` int(11) NOT NULL AUTO_INCREMENT,
   `titreProjet` varchar(90) NOT NULL,
   `descriptionProjet` longtext NOT NULL,
-  `brandingProjet` varchar(200) DEFAULT NULL,
-  `positionnementProjet` longtext,
-  `identiteProjet` varchar(200) DEFAULT NULL,
-  `referencesProjet` longtext,
-  `dontlikeProjet` longtext,
-  `commentaireProjet` longtext,
   `isActiveProjet` tinyint(1) DEFAULT '0',
   `idUtilisateur` int(11) NOT NULL,
   `tailleEntreprise` int(11) NOT NULL,
@@ -174,17 +176,17 @@ CREATE TABLE IF NOT EXISTS `projets` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
--- Dumping data for table `projets`
+-- Contenu de la table `projets`
 --
 
-INSERT INTO `projets` (`idProjet`, `titreProjet`, `descriptionProjet`, `brandingProjet`, `positionnementProjet`, `identiteProjet`, `referencesProjet`, `dontlikeProjet`, `commentaireProjet`, `isActiveProjet`, `idUtilisateur`, `tailleEntreprise`, `caEntreprise`, `ptsContactEntreprise`, `optionProjet`, `nbARProjet`, `nbDesignerSouhaite`, `idPack`) VALUES
-(1, 'Emoson', 'Lorem ipsum', NULL, NULL, NULL, NULL, NULL, NULL, 1, 2, 3, 2, '["1","2","7","8"]', '3', 3, 5, 1),
-(2, 'Emosonvrvtcde', 'dioe efde', NULL, NULL, NULL, NULL, NULL, NULL, 1, 2, 2, 3, '["3","4","5"]', '2', 3, 3, 1);
+INSERT INTO `projets` (`idProjet`, `titreProjet`, `descriptionProjet`, `isActiveProjet`, `idUtilisateur`, `tailleEntreprise`, `caEntreprise`, `ptsContactEntreprise`, `optionProjet`, `nbARProjet`, `nbDesignerSouhaite`, `idPack`) VALUES
+(1, 'Emoson', 'Lorem ipsum', 1, 2, 3, 2, '["1","2","7","8"]', '["1","2"]', 3, 5, 1),
+(2, 'Emosonvrvtcde', 'dioe efde', 1, 2, 2, 3, '["3","4","5"]', '["1","2"]', 3, 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `propose`
+-- Structure de la table `propose`
 --
 
 CREATE TABLE IF NOT EXISTS `propose` (
@@ -198,17 +200,18 @@ CREATE TABLE IF NOT EXISTS `propose` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `propose`
+-- Contenu de la table `propose`
 --
 
 INSERT INTO `propose` (`idUtilisateur`, `idProjet`, `acceptation`, `validation`) VALUES
 (3, 1, 1, 1),
-(3, 2, 2, 0);
+(3, 2, 2, 0),
+(5, 1, 0, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateurs`
+-- Structure de la table `utilisateurs`
 --
 
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
@@ -226,21 +229,22 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   PRIMARY KEY (`idUtilisateur`),
   UNIQUE KEY `loginUtilisateur_UNIQUE` (`loginUtilisateur`),
   KEY `fk_utilisateurs_comptes_lies1_idx` (`idCompte`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
--- Dumping data for table `utilisateurs`
+-- Contenu de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`idUtilisateur`, `nomUtilisateur`, `prenomUtilisateur`, `telUtilisateur`, `loginUtilisateur`, `passUtilisateur`, `emailUtilisateur`, `roleUtilisateur`, `bioUtilisateur`, `idCompte`, `certifUtilisateur`) VALUES
 (1, 'AdminTest', 'testeur', 5478987, 'admin', 'c759eaf09e4638954f63ace0ce1b53b40f62ccb7', 'test@test.com', 'ADMIN', NULL, NULL, 0),
 (2, 'Dupré', 'dehde', 1456421212, 'entreprise', '596fed20aa89037d670e419403c205068d484654', 'dejhe@hyegd.de', 'ENTREPRISE', 'derf r fer', NULL, 1),
-(3, 'Delarue', 'Jean', 1242545214, 'designer', '3cffd736dfac1e79687f168cf697611b35060da3', 'hde@test.fr', 'GRAPHISTE', NULL, NULL, 1);
+(3, 'Delarue', 'Jean', 1242545214, 'designer', '3cffd736dfac1e79687f168cf697611b35060da3', 'hde@test.fr', 'GRAPHISTE', NULL, NULL, 1),
+(5, 'Peter ', 'Prasanna', 148659663, 'aa', 'cabdd66378e5d2df5338958ebde9d305fad44def', 'peter.prasana@gmail.com', 'GRAPHISTE', 'ergregt', NULL, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `widgets_lies`
+-- Structure de la table `widgets_lies`
 --
 
 CREATE TABLE IF NOT EXISTS `widgets_lies` (
@@ -253,50 +257,56 @@ CREATE TABLE IF NOT EXISTS `widgets_lies` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables exportées
 --
 
 --
--- Constraints for table `commentaires`
+-- Contraintes pour la table `commentaires`
 --
 ALTER TABLE `commentaires`
   ADD CONSTRAINT `fk_utilisateurs_has_projets_projets1` FOREIGN KEY (`idProjet`) REFERENCES `projets` (`idProjet`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_utilisateurs_has_projets_utilisateurs1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`idUtilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `entreprises`
+-- Contraintes pour la table `compte_soundcloud`
+--
+ALTER TABLE `compte_soundcloud`
+  ADD CONSTRAINT `compte_soundcloud_ibfk_1` FOREIGN KEY (`compte_soundcloud_designer_id`) REFERENCES `utilisateurs` (`idUtilisateur`);
+
+--
+-- Contraintes pour la table `entreprises`
 --
 ALTER TABLE `entreprises`
   ADD CONSTRAINT `fk_entreprises_utilisateurs1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`idUtilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `fichiers_lies`
+-- Contraintes pour la table `fichiers_lies`
 --
 ALTER TABLE `fichiers_lies`
   ADD CONSTRAINT `fk_fichiers_lies_commentaires1` FOREIGN KEY (`idUtilisateur`) REFERENCES `commentaires` (`idUtilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_fichiers_lies_projets1` FOREIGN KEY (`idProjet`) REFERENCES `projets` (`idProjet`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `projets`
+-- Contraintes pour la table `projets`
 --
 ALTER TABLE `projets`
   ADD CONSTRAINT `fk_projets_pack1` FOREIGN KEY (`idPack`) REFERENCES `pack` (`idPack`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `propose`
+-- Contraintes pour la table `propose`
 --
 ALTER TABLE `propose`
   ADD CONSTRAINT `propose_ibfk_1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`idUtilisateur`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `propose_ibfk_2` FOREIGN KEY (`idProjet`) REFERENCES `projets` (`idProjet`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `utilisateurs`
+-- Contraintes pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
   ADD CONSTRAINT `fk_utilisateurs_comptes_lies1` FOREIGN KEY (`idCompte`) REFERENCES `comptes_lies` (`idCompte`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `widgets_lies`
+-- Contraintes pour la table `widgets_lies`
 --
 ALTER TABLE `widgets_lies`
   ADD CONSTRAINT `fk_widgets_lies_utilisateurs1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateurs` (`idUtilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION;
