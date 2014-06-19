2255d4543524865eca8e641b4d43794ed2ae476e
Leroy Valentin <valentin.ipod@gmail.com>
2014-06-19 16:39:06 +0200

    Correctif

--- views/projet/voir_projet.php
+++ views/projet/voir_projet.php
@@ -30,20 +30,21 @@
     <a class="btn btn-large btn-info" href="index.php?module=projet&action=voir_page_projet&idProjet=<?php echo $idProjet; ?>" role="button"><i class="fa fa-file-text"></i> <span>Page du projet</span></a>
     <a class="btn btn-large btn-warning" href="index.php?module=projet&action=voir_projet&idProjet=<?php echo $idProjet; ?>" role="button"><i class="fa fa-file"></i> <span>Voir brief</span></a>
     <a class="btn btn-large btn-primary" href="index.php?module=projet&action=voir_contrat&idProjet=<?php echo $idProjet; ?>" role="button"><i class="fa fa-file"></i> <span>Contrat</span></a>
-    <hr>
+    
 
     </center>
-
-    <h2>Détail du projet</h2>
-    <p><strong>Titre projet : </strong><?php echo $get_projet['titreProjet']; ?></p>
-    <p><strong>Description projet : </strong><?php echo $get_projet['descriptionProjet'];?></p>
-    <p><strong>Strategie de branding : </strong><?php echo 'public/brief_formulaire/branding/'.$get_projet['brandingProjet'];?></p>
-    <p><strong>Positionnement : </strong><?php echo $get_projet['positionnementProjet'];?></p>
-    <p><strong>Identité visuelle : </strong><?php echo 'public/brief_formulaire/identite/'.$get_projet['identiteProjet'];?></p>
-    <p><strong>Références musicales : </strong><?php echo $get_projet['referencesProjet'];?></p>
-    <p><strong>Ce que vous ne souhaitez pas : </strong><?php echo $get_projet['dontlikeProjet'];?></p>
-    <p><strong>Commentaires : </strong><?php echo $get_projet['commentaireProjet'];?></p>
-    <p><strong>Pack : </strong><?php echo $get_projet['titrePack'];?></p>
+        <br>
+    <div class="well" style="">
+        <h2>Détail du projet</h2>
+   <?php
+        if(!empty($get_projet['descriptionProjet'])){ echo '<p><strong>Description projet : </strong>'.$get_projet['descriptionProjet'].'</p>';}
+        if(!empty($get_projet['brandingProjet'])){echo '<p><strong>Branding projet : </strong>'.$get_projet['brandingProjet'].'</p>';}
+	if(!empty($get_projet['positionnementProjet'])){echo '<p><strong>Positionnement projet : </strong>'.$get_projet['positionnementProjet'].'</p>';}
+	if(!empty($get_projet['identiteProjet'])){echo '<p><strong>Identité : </strong>'.$get_projet['identiteProjet'].'</p>';}
+	if(!empty($get_projet['referencesProjet'])){echo '<p><strong>Réferences : </strong>'.$get_projet['referencesProjet'].'</p>';}
+	if(!empty($get_projet['dontlikeProjet'])){echo '<p><strong>Ce que vous ne voulez pas voir : </strong>'.$get_projet['dontlikeProjet'].'</p>';}
+	if(!empty($get_projet['commentaireProjet'])){echo '<p><strong>Commentaire projet : </strong>'.$get_projet['commentaireProjet'].'</p>';}?>
+   <?php if(!empty($get_projet['identiteProjet'])){ ?><p><strong>Pack : </strong><?php echo $get_projet['titrePack'];?></p> <?php } ?>
     <p><strong>Voix off : </strong><?php
             if ($get_projet['optionProjet'] == "1"){
                 echo "Entre 1 à 5 messages par mois";
@@ -73,10 +74,12 @@
                 {
                     echo "<span class='label label-info'> Terminé </span>";
                 }?></strong></p>
-    <hr>
-    <h2>Information de l'entreprise</h2>
+    
+    </div>
+  
     <br>
     <div class="well">
+          <h2>Information de l'entreprise</h2>
         <p><strong>Nom : </strong><?php echo $get_projet['nomUtilisateur']; ?></p>
         <p><strong>Prénom : </strong><?php echo $get_projet['prenomUtilisateur']; ?></p>
         <p><strong>Email  : </strong><a href="mailto:<?php echo $get_projet['emailUtilisateur']; ?>"><?php echo $get_projet['emailUtilisateur'];?></a></p>
